fa267d47f134bdede8c5f291ad0ca1220267bca3
Erukaron <45532997+Erukaron@users.noreply.github.com>
2021-03-09 22:45:18 +0100

    Shell
    -Error correction for com calls -> stack messed up
    -Always use upper case letters when working with the root dir
    -adjusted boot.asm so that other os recognize the file system
    -integrated dir.com into shell
    -cls for shell
    -exit for shell
    -halt for shell

--- src/COM/SHELL.asm
+++ src/COM/SHELL.asm
@@ -29,8 +29,47 @@ jmp MAIN
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
+; Constants
+;-------------------------------------------------------------------------------
+INPUT_BUFFER_LENGTH     equ 80
+
+FAT_RD_ENTRY_LENGTH         equ 32
+FAT_RD_NAME_OFFSET          equ 0x00
+FAT_RD_EXT_OFFSET           equ 0x08
+FAT_RD_ATTRIB_OFFSET        equ 0x0b
+    FAT_RD_ATTRIB_READ_ONLY_OFFSET      equ 0x01
+    FAT_RD_ATTRIB_HIDDEN_OFFSET         equ 0x02
+    FAT_RD_ATTRIB_SYSTEM_OFFSET         equ 0x04
+    FAT_RD_ATTRIB_VOLUME_LBL_OFFSET     equ 0x08
+    FAT_RD_ATTRIB_SUB_DIR_OFFSET        equ 0x10
+    FAT_RD_ATTRIB_ARCHIVE_OFFSET        equ 0x20
+    FAT_RD_ATTRIB_DEVICE_OFFSET         equ 0x40
+    FAT_RD_ATTRIB_UNUSED                equ 0x80
+FAT_RD_CLUSTER_OFFSET       equ 0x1a
+FAT_RD_SIZE_OFFSET          equ 0x1c
+
+MAX_ROOT_ENTRIES            equ 224
+
+DIR_SIZE_OFFSET             equ 19
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
 ; Vars
 ;-------------------------------------------------------------------------------
+welcome_msg             db "TONY shell program loaded.", ASCII_LF, ASCII_LF, 0
+current_directory       times 45 db 0
+                        db 0
+ready_indicator         db ">", 0
+
+input_buffer            times INPUT_BUFFER_LENGTH db 0
+split_buffer            times INPUT_BUFFER_LENGTH db 0
+
+cmd_file_not_found      db "Command or file not found!", ASCII_LF, 0
+
+com_ident               db ".COM", 0
+
+run                     db TRUE
+
 enable_hacking_game_ident db "-fallout_style"
 enable_hacking_game       db FALSE
 
@@ -47,6 +86,15 @@ fo_use_sound             db FALSE
 
 fo_fast_mode_ident       db "-fast", 0
 fo_fast_mode             db FALSE
+
+cmd_dir_ident            db "dir", 0
+cmd_cd_ident             db "cd", 0
+cmd_cls_ident            db "cls", 0
+cmd_color_ident          db "color", 0
+cmd_exit_ident           db "exit", 0
+cmd_path_ident           db "path", 0
+cmd_set_path_ident       db "set path", 0
+cmd_halt_ident           db "halt", 0
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
@@ -125,9 +173,397 @@ MAIN:
         call fallout_hacking_game
     .skip_hacking_game:
 
+    mov si, welcome_msg
+    int 0x91 ; print
 
+    jmp shell_loop
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+shell_loop:
+    mov si, current_directory
+    int 0x91
+    mov si, ready_indicator
+    int 0x91
+
+    mov al, 0
+    mov cx, INPUT_BUFFER_LENGTH
+    mov di, split_buffer
+    int 0xd1 ; intiailze string
+    mov di, input_buffer
+    int 0xd1 ; intiailze string
+    int 0xb1 ; readln
+
+    cmp byte [es:di], 0
+    jz shell_loop
+
+    mov si, input_buffer
+    mov di, split_buffer
+    mov ah, 0b10 ; terminate first and second string
+    mov al, ASCII_SP
+    mov cx, INPUT_BUFFER_LENGTH
+    int 0xd4 ; split string
+
+    call try_exec_file
+    jnc .check_run
+
+    call try_exec_cmd
+    jnc .check_run
+
+    mov al, 2 ; error prefix
+    mov si, cmd_file_not_found
+    int 0x97 ; prefix print
+
+    .check_run:
+        cmp byte [cs:run], TRUE
+        jz shell_loop
 
     ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Searches for a executable file with its name in input_buffer
+; Executes it, if it exists, giving it arguments in split_buffer
+; input:
+;   input_buffer -> Filename
+;   split_buffer -> File arguments
+; ouput:
+;   carry flag
+;       -> set, if file does not exist
+;       -> clear, if file executed
+;-------------------------------------------------------------------------------
+try_exec_file:
+    ; check if file is executable
+    mov si, input_buffer
+    mov ah, 0b10 ; contains substring, ignore case
+    mov di, com_ident
+    int 0xd2 ; check if string contains substring
+    jc .file_non_existant
+
+    ; si is set to input buffer
+    mov di, split_buffer
+    int 0xf2 ; call com program
+    cmp ax, 0x01 ; file not found error
+    jz .file_non_existant
+    clc
+    jmp .done
+
+    .file_non_existant:
+        stc
+    .done:
+        ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Searches for the command in input_buffer and executes it giving the parameters
+; in split_buffer
+; input:
+;   input_buffer -> Commandname
+;   split_buffer -> Command arguments
+; ouput:
+;   carry flag
+;       -> set, if command does not exist
+;       -> clear, if command executed
+;-------------------------------------------------------------------------------
+try_exec_cmd:
+    push word .cmd_return_point ; commands exit via ret instruction
+                                ; they should all return to this central point
+
+    ; check commands
+    mov si, input_buffer
+    mov ah, 0b11 ; ignore case, string equals
+
+    mov di, cmd_dir_ident
+    int 0xd2 ; check if string contains substring
+    jnc dir
+
+    mov di, cmd_cd_ident
+    int 0xd2 ; check if string contains substring
+    jnc cd
+
+    mov di, cmd_cls_ident
+    int 0xd2 ; check if string contains substring
+    jnc cls
+
+    mov di, cmd_color_ident
+    int 0xd2 ; check if string contains substring
+    jnc color
+
+    mov di, cmd_exit_ident
+    int 0xd2 ; check if string contains substring
+    jnc exit
+
+    mov di, cmd_path_ident
+    int 0xd2 ; check if string contains substring
+    jnc path
+
+    mov di, cmd_set_path_ident
+    int 0xd2 ; check if string contains substring
+    jnc set_path
+
+    mov di, cmd_halt_ident
+    int 0xd2 ; check if string contains substring
+    jnc halt
+
+    jmp .cmd_non_existant
+
+    .cmd_return_point:
+    clc
+    jmp .done
+
+    .cmd_non_existant:
+        pop ax ; command not called, remove return address from stack
+        stc
+    .done:
+        ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Displays the contents of the current directory
+;-------------------------------------------------------------------------------
+dir:
+    push ds
+        push word ROOT_SEGMENT
+        pop ds
+
+        mov byte [cs:.file_flags], 0
+        
+        ; Display system volume information
+        mov si, ROOT_OFFSET
+        mov al, FAT_RD_ATTRIB_ARCHIVE_OFFSET + FAT_RD_ATTRIB_VOLUME_LBL_OFFSET
+        mov cx, MAX_ROOT_ENTRIES
+        .serach_volume_info:
+            cmp byte [ds:si], 0
+            jz .volume_label_done ; no more entries
+
+            cmp [ds:si + FAT_RD_ATTRIB_OFFSET], al
+            jz .display_volume_info
+
+            add si, FAT_RD_ENTRY_LENGTH
+        loop .serach_volume_info
+        .display_volume_info:
+            push ds
+            push si
+                push cs
+                pop ds
+                mov si, .volume_label
+                int 0x91 ; print
+            pop si
+            pop ds
+
+            mov cx, FAT_RD_ATTRIB_OFFSET ; length of volume label
+            .putch_volume_info:
+                lodsb
+                int 0x90 ; putch
+            loop .putch_volume_info
+
+            mov al, ASCII_LF
+            int 0x90 ; linebreak
+        .volume_label_done:
+
+
+        ; Display any subdirectory (if any)
+        mov si, ROOT_OFFSET
+        mov cx, MAX_ROOT_ENTRIES
+        .serach_directories:
+            cmp byte [ds:si], 0
+            jz .directories_done ; no more entries
+
+            mov al, FAT_RD_ATTRIB_SUB_DIR_OFFSET
+            or al, [ds:.file_flags]
+            cmp [ds:si + FAT_RD_ATTRIB_OFFSET], al
+            jnz .next_directory
+
+            ; display directory
+            push cx
+                mov cx, FAT_RD_ATTRIB_OFFSET ; length of directory name
+                .putch_directory_name:
+                    lodsb
+                    int 0x90 ; putch
+                loop .putch_directory_name
+            pop cx
+
+            push ds
+            push si
+                push cs
+                pop ds
+                mov si, .dir_label
+                int 0x91 ; print
+            pop si
+            pop ds
+
+            mov al, ASCII_LF
+            int 0x90 ; linebreak
+
+            .next_directory:
+                add si, FAT_RD_ENTRY_LENGTH
+        loop .serach_directories
+        .directories_done:
+
+        ; Display any file matching the file flag (if any) with its size
+        mov si, ROOT_OFFSET
+        mov cx, MAX_ROOT_ENTRIES
+        .serach_files:
+            cmp byte [ds:si], 0
+            jz .files_done ; no more entries
+
+            mov bp, si
+
+            mov al, [cs:.file_flags]
+            cmp [ds:si + FAT_RD_ATTRIB_OFFSET], al
+            jnz .next_file
+
+            ; display filename
+            push cx
+                mov cx, FAT_RD_EXT_OFFSET ; length of file name
+                .putch_file_name:
+                    lodsb
+                    int 0x90 ; putch
+                loop .putch_file_name
+            pop cx
+
+            mov al, ASCII_SP
+            int 0x90 ; putch
+
+            ; display filextension
+            push cx
+                mov cx, FAT_RD_ATTRIB_OFFSET - FAT_RD_EXT_OFFSET ; length of file name
+                .putch_file_ext:
+                    lodsb
+                    int 0x90 ; putch
+                loop .putch_file_ext
+            pop cx
+
+            ; Display size (build size from least significant digit to most significant digit)
+            call .fill_size_str
+
+            mov ah, 1
+            int 0x98 ; get current position
+            mov bx, DIR_SIZE_OFFSET
+            int 0x99 ; set current position
+
+            push ds
+            push si
+                push cs
+                pop ds
+                mov si, .size_str
+                int 0x91 ; print
+            pop si
+            pop ds
+
+            mov al, ASCII_LF
+            int 0x90 ; linebreak
+
+            .next_file:
+                mov si, bp
+                add si, FAT_RD_ENTRY_LENGTH
+        loop .serach_files
+        .files_done:
+
+    pop ds
+    ret
+
+    .file_flags     db 0
+    .volume_label   db 'Volume label: ', 0
+    .dir_label      db ' <DIR> ', 0
+    .size_str       times 8 db 0
+                    db 0 ; zero termination for size str
+
+    ; Fills the size_str with the size of the currently addressed file via ds:bp
+    ; File size is filled with leading spaces
+    .fill_size_str:
+        pusha
+        push es
+            push cs
+            pop es
+
+            mov di, .size_str
+
+            mov byte [cs:.display_zeros], FALSE ; do not display leading zeros
+            add bp, FAT_RD_SIZE_OFFSET + 3 ; offset to most significant byte
+
+            mov cx, 4
+
+            .fill_str:
+                mov dl, [ds:bp]
+                int 0xe4 ; transform to printable hex format
+
+                cmp byte [cs:.display_zeros], FALSE
+                jnz .write_to_var
+                    ; If entries are zero, write space instead
+                    cmp dh, ASCII_0
+                    jnz .write_to_var
+                    mov dh, ASCII_SP
+
+                    cmp dl, ASCII_0
+                    jnz .write_to_var
+                    mov dl, ASCII_SP
+                .write_to_var:
+                mov al, dh
+                stosb
+                mov al, dl
+                stosb
+                
+                dec bp
+                cmp dx, ASCII_SP * 256 + ASCII_SP
+                jz .next
+                    mov byte [cs:.display_zeros], TRUE ; there was at least one character printed, spaces can no longer be omitted!
+                .next:
+            loop .fill_str
+        pop es
+        popa
+        ret
+
+        .display_zeros db 0
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+cd:
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Clears the screen
+;-------------------------------------------------------------------------------
+cls:
+    int 0x95
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+color:
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Shell returns control to caller after command is interpreted
+;-------------------------------------------------------------------------------
+exit:
+    mov byte [cs:run], FALSE
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+path:
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+;-------------------------------------------------------------------------------
+set_path:
+    ret
+;-------------------------------------------------------------------------------
+
+;-------------------------------------------------------------------------------
+; Halts the machine
+;-------------------------------------------------------------------------------
+halt:
+    cli
+    hlt
+    ret ; test if halt behaves like intended on real hardware
 ;-------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
